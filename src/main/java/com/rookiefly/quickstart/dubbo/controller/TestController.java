33528e7ad7ba715c13c268176b6f46259a89413d
rookiefly <datouxiangzi@163.com>
2021-05-14 10:50:36 +0800

    test async servlet

---
+++ src/main/java/com/rookiefly/quickstart/dubbo/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.rookiefly.quickstart.dubbo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+
+    @GetMapping("call/{name}")
+    public DeferredResult<String> testSync(@PathVariable String name) {
+        final DeferredResult<String> deferredResult = new DeferredResult<>(5000L);
+        EXECUTOR_SERVICE.execute(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            deferredResult.setResult(String.format("Hello %s", name));
+        });
+        // deferredResult.setResult(String.format("Hello %s", name));
+        return deferredResult;
+    }
+}
