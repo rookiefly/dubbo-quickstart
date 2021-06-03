3f3cb9889314e81ef867ccfa42f8e0a46c60e2dc
rookiefly <datouxiangzi@163.com>
2021-06-03 16:50:03 +0800

    add sharding-jdbc

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -64,22 +64,24 @@ create table if not exists city_data
 -- ----------------------------
 DROP SCHEMA IF EXISTS ds0;
 DROP SCHEMA IF EXISTS ds1;
+CREATE SCHEMA IF NOT EXISTS ds0;
+CREATE SCHEMA IF NOT EXISTS ds1;
 
-CREATE TABLE IF NOT EXISTS ds0.t_order
+CREATE TABLE IF NOT EXISTS ds0.t_order_0
 (
     order_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id  INT    NOT NULL,
     status   VARCHAR(50),
     PRIMARY KEY (order_id)
 );
-CREATE TABLE IF NOT EXISTS ds1.t_order
+CREATE TABLE IF NOT EXISTS ds1.t_order_0
 (
     order_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id  INT    NOT NULL,
     status   VARCHAR(50),
     PRIMARY KEY (order_id)
 );
-CREATE TABLE IF NOT EXISTS ds0.t_order_item
+CREATE TABLE IF NOT EXISTS ds0.t_order_item_0
 (
     order_item_id BIGINT NOT NULL AUTO_INCREMENT,
     order_id      BIGINT NOT NULL,
@@ -87,7 +89,38 @@ CREATE TABLE IF NOT EXISTS ds0.t_order_item
     status        VARCHAR(50),
     PRIMARY KEY (order_item_id)
 );
-CREATE TABLE IF NOT EXISTS ds1.t_order_item
+CREATE TABLE IF NOT EXISTS ds1.t_order_item_0
+(
+    order_item_id BIGINT NOT NULL AUTO_INCREMENT,
+    order_id      BIGINT NOT NULL,
+    user_id       INT    NOT NULL,
+    status        VARCHAR(50),
+    PRIMARY KEY (order_item_id)
+);
+
+CREATE TABLE IF NOT EXISTS ds0.t_order_1
+(
+    order_id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id  INT    NOT NULL,
+    status   VARCHAR(50),
+    PRIMARY KEY (order_id)
+);
+CREATE TABLE IF NOT EXISTS ds1.t_order_1
+(
+    order_id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id  INT    NOT NULL,
+    status   VARCHAR(50),
+    PRIMARY KEY (order_id)
+);
+CREATE TABLE IF NOT EXISTS ds0.t_order_item_1
+(
+    order_item_id BIGINT NOT NULL AUTO_INCREMENT,
+    order_id      BIGINT NOT NULL,
+    user_id       INT    NOT NULL,
+    status        VARCHAR(50),
+    PRIMARY KEY (order_item_id)
+);
+CREATE TABLE IF NOT EXISTS ds1.t_order_item_1
 (
     order_item_id BIGINT NOT NULL AUTO_INCREMENT,
     order_id      BIGINT NOT NULL,
