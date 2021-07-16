a42fcb6af4911e255d57c86c5e69354eca9bc44a
rookiefly <datouxiangzi@163.com>
2021-07-16 11:45:45 +0800

    test redis Order Id Generator

--- src/test/java/com/rookiefly/quickstart/dubbo/DubboQuickstartApplicationTests.java
+++ src/test/java/com/rookiefly/quickstart/dubbo/DubboQuickstartApplicationTests.java
@@ -1,13 +1,25 @@
 package com.rookiefly.quickstart.dubbo;
 
+import com.rookiefly.quickstart.dubbo.service.OrderIdGeneratorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class DubboQuickstartApplicationTests {
 
+    @Resource
+    private OrderIdGeneratorService orderIdGeneratorService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testOrderIdGenerator() {
+        String orderId = orderIdGeneratorService.generateOrderId();
+        System.out.println(orderId);
     }
 
 }
